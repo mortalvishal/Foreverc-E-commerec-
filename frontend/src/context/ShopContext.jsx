25f0a09ad97619187e973a5d8bf35d78a475e84d
vishal pathak <vishalpathak842@gmail.com>
2025-03-08 09:25:03 +0530

    Add ShopContext for managing product data and currency settings

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import { products } from "../assets/assets";
+
+export const ShopContext = createContext();
+
+const ShopContextProvider =(props)=>{
+    const currency = '₹';
+    const delivery_fee = 99;
+    const value = {
+        products,currency,delivery_fee
+    }
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
