0d174a0ef8735769d6330a534d79eaff5019f86e
vishal pathak <vishalpathak842@gmail.com>
2025-03-11 07:42:34 +0530

    Add getCartCount function to ShopContext for calculating total cart items

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -16,7 +16,6 @@ const ShopContextProvider =(props)=>{
             toast.error('Select Product Size')
             return;
         }
-
         let cartData = structuredClone(cartItems);
         if (cartData[itemId]) {
             if (cartData[itemId][size]) {
@@ -33,15 +32,27 @@ const ShopContextProvider =(props)=>{
         setCartItems(cartData);
     }
 
-    useEffect(()=>{
-        console.log(cartItems);
-        
-    },[cartItems])
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if (cartItems[items][item] >0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) {
+                    
+                }
+            }
+        }
+        return totalCount;
+    }
 
     const value = {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
-        cartItems,addToCart
+        cartItems,addToCart,
+        getCartCount
     }
     return (
         <ShopContext.Provider value={value}>
