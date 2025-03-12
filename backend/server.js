b4ae73a5c2da1ca73c382b299849025f9709ca4d
vishal pathak <vishalpathak842@gmail.com>
2025-03-12 14:16:13 +0530

    Add product routes to the backend server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/user.routes.js';
+import productRouter from './routes/product.routes.js';
 
 
 // App Config
@@ -22,6 +23,7 @@ app.use(cors());
 
 // API endpoints
 app.use('/api/user',userRouter);
+app.use('/api/product',productRouter);
 
 app.get('/',(req,res)=>{
     res.send("API Working")
