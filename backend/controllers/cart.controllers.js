cecc052b1cc338b572b2de86ce7316870cf03d69
vishal pathak <vishalpathak842@gmail.com>
2025-03-17 15:01:07 +0530

    Add cart controller with functions to manage user cart operations

---
+++ backend/controllers/cart.controllers.js
@@ -0,0 +1,18 @@
+
+
+// add products to user cart
+const addToCart = async() =>{
+
+}
+
+// update products to user cart
+const updateCart = async() =>{
+
+}
+
+// get user cart
+const getUserCart = async() =>{
+
+}
+
+export {addToCart,updateCart,getUserCart}
