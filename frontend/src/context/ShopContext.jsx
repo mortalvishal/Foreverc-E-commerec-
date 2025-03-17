c143f2120274a0e59499ccdccc78914fbc8ca204
vishal pathak <vishalpathak842@gmail.com>
2025-03-17 15:46:45 +0530

    Add cart synchronization with backend API for adding, updating, and retrieving cart items

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -36,6 +36,15 @@ const ShopContextProvider =(props)=>{
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(backendUrl+ '/api/cart/add',{itemId,size},{headers:{token}})
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)                
+            }
+        }
     }
 
     const getCartCount = ()=>{
@@ -60,6 +69,14 @@ const ShopContextProvider =(props)=>{
 
         cartData[itemId][size] = quantity;
         setCartItems(cartData);
+        if (token) {
+            try {
+                await axios.post(backendUrl+ '/api/cart/update' , {itemId,size,quantity},{headers:{token}})
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartAmount = ()=>{
@@ -94,6 +111,18 @@ const ShopContextProvider =(props)=>{
         }
     }
 
+    const getUserCart = async (token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get',{},{headers:{token}})
+            if (response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
         if (backendUrl) {
             getProductsData();
@@ -104,6 +133,7 @@ const ShopContextProvider =(props)=>{
     useEffect(()=>{
         if (!token && localStorage.getItem('token')) {
             setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
         }
     },[])
 
