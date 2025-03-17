06e56144e5d6e1cb94a02b1bd507ce4290a98464
vishal pathak <vishalpathak842@gmail.com>
2025-03-17 13:57:37 +0530

    Set default backendUrl to 'http://localhost:4000' if not provided

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -8,7 +8,7 @@ export const ShopContext = createContext();
 const ShopContextProvider =(props)=>{
     const currency = '₹';
     const delivery_fee = 99;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000'
     const[search,setSearch]= useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
