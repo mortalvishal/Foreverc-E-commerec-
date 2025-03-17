734a2e3d8f9564d915ef1b39a0cd74152d201475
vishal pathak <vishalpathak842@gmail.com>
2025-03-17 12:10:17 +0530

    Add product fetching functionality to ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,17 +1,20 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export const ShopContext = createContext();
 
 const ShopContextProvider =(props)=>{
     const currency = '₹';
     const delivery_fee = 99;
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const[search,setSearch]= useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
+    const [products,setProducts] = useState([])
     const navigate = useNavigate();
+    
 
     const addToCart = async (itemId, size) => {
         if (!size) {
@@ -75,6 +78,27 @@ const ShopContextProvider =(props)=>{
         return totalAmount;
     }
 
+    const getProductsData = async () => {
+        try {
+            
+            const response = await axios.get(backendUrl + '/api/product/list')
+            if (response.data.success) {
+                setProducts(response.data.products)
+            }
+            else{
+                toast.error(response.data.message)
+            }
+            
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+    useEffect(()=>{
+        getProductsData();
+    },[])
+
     const value = {
         products,currency,delivery_fee,
         search,setSearch,showSearch,setShowSearch,
@@ -82,7 +106,8 @@ const ShopContextProvider =(props)=>{
         getCartCount,
         updateQuantity,
         getCartAmount,
-        navigate
+        navigate,
+        backendUrl
     }
     return (
         <ShopContext.Provider value={value}>
