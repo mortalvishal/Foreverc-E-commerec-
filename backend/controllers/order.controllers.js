f1e118bca7d53d4d8797abc2a0762cbf130bfcc6
vishal pathak <vishalpathak842@gmail.com>
2025-03-18 12:22:16 +0530

    Add order controller methods for various payment options and order management

---
+++ backend/controllers/order.controllers.js
@@ -0,0 +1,34 @@
+
+// Placing orders using COD(Cash On Delivery) method
+const placeOrder = async(req,res) =>{
+
+}
+
+// Placing orders using Stripe method
+const placeOrderStripe = async(req,res) =>{
+
+}
+
+
+// Placing orders using Razorpay method
+const placeOrderRazorpay = async(req,res) =>{
+
+}
+
+// All orders data for admin panel
+const allOrders = async(req,res) => {
+
+}
+
+// User order data for the Frontend
+const userOrders = async(req,res)=>{
+
+}
+
+// Update order Status from admin panel
+const updateStatus = async(req,res) =>{
+
+}
+
+export {placeOrder,placeOrderStripe,placeOrderRazorpay,allOrders,userOrders,updateStatus}
+
