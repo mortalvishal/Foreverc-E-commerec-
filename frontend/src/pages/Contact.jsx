7e158231508d624d38de9204fae4463087f3bf5b
vishal pathak <83800699+mortalvishal@users.noreply.github.com>
2025-07-11 15:29:13 +0530

    Update Contact.jsx
    There are some minor mistakes, such is Spelling mistake

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
           <p className='text-gray-500'>Phone: (+91) 9876589769  <br/> Email: vishal@forever.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
-          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 rounded'>Exploe Career</button>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 rounded'>Explore Career</button>
         </div>
 
       </div>
